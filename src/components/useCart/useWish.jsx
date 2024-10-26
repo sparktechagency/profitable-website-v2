0f9798962d11c7920a4609fe580fe5c7b13f9efd
foisal <foisalrk2@gmail.com>
2024-10-26 18:56:05 +0600

    sdfsdfdsf

--- src/components/useCart/useWish.jsx
+++ src/components/useCart/useWish.jsx
@@ -1,19 +1,29 @@
-"use client"; 
+"use client";
 
 import { useState, useEffect } from "react";
+
 const useWish = () => {
   const [wishItems, setwishItems] = useState([]);
+
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem("wish")) || [];
     setwishItems(items);
   }, []);
+
   const addToWish = (item) => {
     const existingItems = JSON.parse(localStorage.getItem("wish")) || [];
     existingItems.push(item);
     localStorage.setItem("wish", JSON.stringify(existingItems));
     setwishItems(existingItems);
   };
-  return { wishItems, addToWish };
+
+  const removeFromWish = (itemId) => {
+    const updatedItems = wishItems.filter((item) => item.id !== itemId);
+    localStorage.setItem("wish", JSON.stringify(updatedItems));
+    setwishItems(updatedItems);
+  };
+
+  return { wishItems, addToWish, removeFromWish };
 };
 
 export default useWish;
