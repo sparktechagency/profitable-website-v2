22e91265088a2e00f7b5e2c47c290f49ff2cddff
Nandan Mishra <vkv4052@gmail.com>
2026-01-27 14:32:21 +0530

    Update code

--- src/app/businesses-for-sale-dubai/page.jsx
+++ src/app/businesses-for-sale-dubai/page.jsx
@@ -8,15 +8,9 @@ import BusinessSale from './BusinessSaleForDubai'
 
 
 export const metadata = {
-<<<<<<< HEAD
   title: "Business for Sale in Dubai | Buy Profitable Business Dubai ",
   description:
     " Explore top business for sale in Dubai - find small & profitable business opportunities, buy a business in Dubai, and invest in thriving ventures today.",
-=======
-  title: "About Us - PBFS | Buy & Sell Businesses Online ",
-  description:
-    " Learn about PBFS, the trusted online marketplace for buying, selling, and investing in profitable businesses. Discover our mission to empower entrepreneurs and business owners worldwide. ",
->>>>>>> 95f6486588c6095631ed7e6c94932ab3f200e4d9
    alternates: {  canonical: "https://profitablebusinessesforsale.com/businesses-for-sale-dubai"
    },
 };
