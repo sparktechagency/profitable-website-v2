3ac648479cae5a2c05b2dc1c7dba3d41902ed940
foisaluddin400 <foisalrk2@gmail.com>
2026-04-10 09:56:12 +0600

    Tailwind problem solved

---
+++ postcss.config.mjs
@@ -0,0 +1,15 @@
+
+
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;    
